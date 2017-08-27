100ef064145c41ac8557d23523335739810fc628
Orie Steele <orie@ohs.io>
2017-08-27 10:24:39 -0500

    home style'

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -5,10 +5,9 @@ import RaisedButton from 'material-ui/RaisedButton';
 import { push } from 'react-router-redux';
 import { store } from '../../store/store';
 
+import { Grid, Row, Col } from 'react-flexbox-grid';
 const transmuteLogo = require('../../images/logo-white.png');
 const habitatLogo = require('../../images/harvey/habitat.png');
-
-
 
 export default class HomePage extends React.Component<any, any> {
     launchDemo(path: string) {
@@ -19,30 +18,32 @@ export default class HomePage extends React.Component<any, any> {
         return (
             <Particle>
                 <div className="cta-c">
-                    <img className="habitatLogo" src={habitatLogo} />
-                    <h3>ETH Relief for Hurricane Harvey Victims</h3>
-                    <h5>ETH4Harvey Campaign Open Aug. 26 - Sept. 25</h5>
-                    <p>
-                        Habitat for Humanity Texas is now accepting donations in Ether (ETH).
-                    Use your ETH to help rebuild the communities and lives affected by Hurricane Harvey.
-                    100% of the proceeds are donated directly to Habitat for Humanity Texas to
-                    fund their long term disaster relief efforts in Harvey's aftermath.
-                    </p>
-
-                 
-                    <div className="btn-c">
-                        <RaisedButton
-                            secondary={true}
-                            label="Donate ETH"
-                            onTouchTap={() => {
-                                this.launchDemo('/donate');
-                            }}
-                        />
-                    </div>
+                    <Grid fluid={true} >
+                        <Row middle="xs" >
+                            <Col xs={12} sm={12}>
+                                <img className="habitatLogo" src={habitatLogo} style={{marginTop: '32px'}}/>
+                                <h3>ETH Relief for Hurricane Harvey Victims</h3>
+                                <h5>ETH4Harvey Campaign Open Aug. 26 - Sept. 25</h5>
+                                <p className="blurb">
+                                    Habitat for Humanity Texas is now accepting donations in Ether (ETH).
+                                    Use your ETH to help rebuild the communities and lives affected by Hurricane Harvey.
+                                    100% of the proceeds are donated directly to Habitat for Humanity Texas to
+                                    fund their long term disaster relief efforts in Harvey's aftermath.
+                                </p>
+                                <RaisedButton
+                                    secondary={true}
+                                    label="Donate ETH"
+                                    onTouchTap={() => {
+                                        this.launchDemo('/donate');
+                                    }}
+                                />
+                                <a href="https://transmute.industries" target="_blank">
+                                    <img className="transmuteLogo" src={transmuteLogo} />
+                                </a>
+                            </Col>
+                        </Row>
+                    </Grid>
                 </div>
-                <a href="https://transmute.industries" target="_blank">
-                    <img className="logo" src={transmuteLogo} />
-                </a>
             </Particle>
         );
     }
